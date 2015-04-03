4b17d5a121df504c6b7a96431eafd1874a409f13
raethlo <roman.rostar@gmail.com>
2015-04-03 13:22:23 +0200

    adds a shared record

---
+++ records.hrl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @author raethlo
+%%% @copyright (C) 2015, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 03. Apr 2015 13:21
+%%%-------------------------------------------------------------------
+-author("raethlo").
+
+-record(item,{ type, name, description="No dedcription provided.", price}).
